3112b442ec936279072ee2220118b645451dfb9a
MOHAN SAI DINESH BODDAPATI <85325733+mohansaidinesh@users.noreply.github.com>
2022-12-31 11:29:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+        if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+                        nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+const navLink = document.querySelectorAll('.nav__link')
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+const sections = document.querySelectorAll('section[id]')
+function scrollActive(){
+    const scrollY = window.pageYOffset
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top');
+    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
+const sr = ScrollReveal({
+    distance: '30px',
+    duration: 1800,
+    reset: true,
+});
+
+sr.reveal(`.home__data, .home__img, 
+           .decoration__data,
+           .accessory__content,
+           .footer__content`, {
+    origin: 'top',
+    interval: 200,
+})
+
+sr.reveal(`.share__img, .send__content`, {
+    origin: 'left'
+})
+
+sr.reveal(`.share__data, .send__img`, {
+    origin: 'right'
+})
